7ccecfc670b0b3da6c8aabf40f0f93e22204f6a0
Mikhail Dyomin <mikhaildyomin@gmail.com>
2018-09-19 17:35:12 +0300

    Add faker gem & seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+%w[blog podcast library].each do |category|
+  1000.times do
+    author = Author.create(full_name: Faker::StarWars.character)
+    post = Post.create(category: category, slug: Faker::Lorem.word, title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph(100))
+    post.authors << author
+  end
+end
